acc106615a011a6cb031e12ab78f93326b17b938
codeWithYashSolanki <solankiyash556@gmail.com>
2022-11-19 22:05:59 +0530

    Search in Duplicate Rotated Array

--- RotatedArray.java
+++ RotatedArray.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class RotatedArray {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {7, 8, 3, 4, 5, 6};
         int target = 2;
-        System.out.println(search(arr, target));
+        System.out.println(findPivotInDuplicates(arr));
     }
     static int search(int[] arr, int target) {
-        int pivot = findPivot(arr);
+        int pivot = findPivotInDuplicates(arr);
 
         if (pivot == -1) {
             return binarySearch(arr, target, 0, arr.length - 1);
@@ -47,7 +47,7 @@ public class RotatedArray {
         }return -1;
     }
 
-    static int findPivot(int[] arr) {
+    static int findPivotInDuplicates(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
 
@@ -60,11 +60,22 @@ public class RotatedArray {
             if (mid > start && arr[mid] < arr[mid - 1]) {
                 return mid - 1;
             }
-            if (arr[mid] <= arr[start]) {
-                end = mid - 1;
-            } else {
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            } else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
+
         }return -1;
     }
 
